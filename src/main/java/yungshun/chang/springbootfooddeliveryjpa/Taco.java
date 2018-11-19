5a985a5be42259aff71dfdf83b97861e3769defb
yungshun317 <seraph317@gmail.com>
2018-11-19 17:35:08 +0800

    feat: annotate the domain as entities

---
+++ src/main/java/yungshun/chang/springbootfooddeliveryjpa/Taco.java
@@ -0,0 +1,34 @@
+package yungshun.chang.springbootfooddeliveryjpa;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+public class Taco {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    private Date createdAt;
+
+    @ManyToMany(targetEntity=Ingredient.class)
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<String> ingredients = new ArrayList<>();
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+}
